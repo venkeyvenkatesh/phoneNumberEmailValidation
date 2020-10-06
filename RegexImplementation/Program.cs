eb8a34f6cd344499456750a269d31f8285a949c5
venkey <venkeyvenkatesh885@gmail.com>
2020-10-06 06:19:40 +0530

    first commit

---
+++ RegexImplementation/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegexImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ENter a pincode number");
+
+            string pinCode = Console.ReadLine();
+
+            while(!(validatePinCode(pinCode)))
+            {
+                Console.WriteLine("Please enter valid pin code");
+                pinCode = Console.ReadLine();
+            }
+            Console.WriteLine("Valide one");
+
+           
+
+        }
+        public static bool validatePinCode(string pinCode)
+        {
+            string pattern = @"(^[a-zA-Z0-9!@#$%^&*()-+=]{6}$)";
+            Regex re = new Regex(pattern);
+            if (re.IsMatch(pinCode))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+       
+    }
+}
